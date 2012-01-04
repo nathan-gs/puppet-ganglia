8841b1dd723a07538ea83a30f0cfa601be334e74
Nathan Bijnens <nathan@nathan.gs>
2012-01-04 10:29:29 +0100

    web: graph.d & view config &  dwoo template directory.

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,7 +1,9 @@
 class ganglia::web (
 	$www_dir = '/var/www/ganglia',
 	$version	= 'ganglia-web',
-	$conf_dir	= '/etc/ganglia/web.d'
+	$conf_dir	= '/etc/ganglia/web.d',
+	$view_conf_dir	= '/etc/ganglia/web.conf.d',
+	$graph_dir	= '/etc/ganglia/web.graph.d'
 ) {
 
 	file { $www_dir : 
@@ -28,14 +30,51 @@ class ganglia::web (
 
 
 	exec { "ganglia-web-sed-version" :
-                command => 'sed -e s/@GWEB_VERSION@/git/ version.php.in > version.php',
-                cwd		=> "${www_dir}",
-                timeout => 3600,
-        }
+        command => 'sed -e s/@GWEB_VERSION@/git/ version.php.in > version.php',
+        cwd		=> "${www_dir}",
+        timeout => 3600,
+    }
 
 	exec { "ganglia-web-sed-conf_default" :
-                command => 'sed -e "s/@varstatedir@/\/var\/lib/" conf_default.php.in > conf_default.php',
-                cwd		=> "${www_dir}",
-                timeout => 3600,
-        }
+        command => 'sed -e "s/@varstatedir@/\/var\/lib/" conf_default.php.in > conf_default.php',
+        cwd		=> "${www_dir}",
+        timeout => 3600,
+    }
+
+    file { "/var/lib/ganglia/dwoo" :
+    	ensure	=> directory,
+		owner 	=> 'www-data',
+		group 	=> 'www-data',
+		mode 	=> 750,
+    }
+
+    file { "${view_conf_dir}" :
+    	ensure	=> directory,
+		owner 	=> 'root',
+		group 	=> 'www-data',
+		mode 	=> 750,
+		source 	=> "puppet:///modules/ganglia/${version}/conf",
+		recurse => true,
+		replace	=> false,
+    }
+
+    file { "${conf_dir}/000-path.php" :
+    	content	=> template('ganglia/web/000-path.php.erb'),
+    	ensure	=> present,
+    	owner 	=> 'root',
+		group 	=> 'www-data',
+		mode 	=> 640,
+		require	=> [File[$conf_dir],File[$view_conf_dir],File[$graph_dir]]
+    }
+
+    file { "${graph_dir}" :
+    	ensure	=> directory,
+		owner 	=> 'root',
+		group 	=> 'www-data',
+		mode 	=> 750,
+		source 	=> "puppet:///modules/ganglia/${version}/graph.d",
+		recurse => true,
+		replace	=> false,
+    }
+
 }
