ed5a052662fca093af58d0a27c180ca7c9fcc70a
Nathan Bijnens <nathan@nathan.gs>
2012-08-21 17:13:46 +0200

    CentOS support

--- manifests/gmon.pp
+++ manifests/gmon.pp
@@ -102,7 +102,7 @@ class ganglia::gmon {
 			group 	=> root,
 			mode	=> 644,
 			require	=> File['/etc/ganglia/gmon.d'],
-			notify	=> Service[$service_name]
+			notify	=> Service[$ganglia::gmon::service_name]
 		}
 		
 	}
@@ -120,7 +120,7 @@ class ganglia::gmon {
 			group 	=> root,
 			mode	=> 644,
 			require	=> File['/etc/ganglia/gmon.d'],
-			notify	=> Service[$service_name]
+			notify	=> Service[$ganglia::gmon::service_name]
 		}
 	}
 
@@ -134,7 +134,7 @@ class ganglia::gmon {
 			group 	=> root,
 			mode	=> 644,
 			require	=> File['/etc/ganglia/gmon.d'],
-			notify	=> Service[$service_name]
+			notify	=> Service[$ganglia::gmon::service_name]
 		}
 	}
 
